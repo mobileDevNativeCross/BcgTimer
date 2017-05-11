59120965a7e456a57f6f0637f5beeda0bfcb5d1a
Vika <vika@iMac-Vika.local>
2017-05-11 11:33:09 +0300

    android

--- android/src/main/java/com/reactlibrary/RNBcgTimerModule.java
+++ android/src/main/java/com/reactlibrary/RNBcgTimerModule.java
@@ -1,6 +1,9 @@
 
 package com.reactlibrary;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -8,7 +11,7 @@ import com.facebook.react.bridge.Callback;
 
 public class RNBcgTimerModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+  private ReactApplicationContext reactContext;
 
   public RNBcgTimerModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -18,5 +21,14 @@ public class RNBcgTimerModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNBcgTimer";
+  }
+
+
+  @ReactMethod
+  public void changeStatus(Boolean status) {
+    SharedPreferences shared = reactContext.getSharedPreferences("myData", Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = shared.edit();
+    editor.putBoolean("status", status);
+    editor.commit();
   }
 }
